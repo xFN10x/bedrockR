2c889913b3ae95b0d282c4f4f20b544028c87b2e
_FN10_ <mathdudex09@gmail.com>
2026-08-10 15:04:56 -0300

    fix the positioning

--- src/main/java/fn10/bedrockr/ui/components/VerticalLabel.java
+++ src/main/java/fn10/bedrockr/ui/components/VerticalLabel.java
@@ -1,32 +1,66 @@
 package fn10.bedrockr.ui.components;
 
+import javax.swing.*;
 import java.awt.*;
-import java.awt.font.FontRenderContext;
 import java.awt.geom.Rectangle2D;
 
-import javax.swing.JLabel;
-
 public class VerticalLabel extends JLabel {
+    private final boolean clockwise;
 
     public VerticalLabel(String text) {
-        super(text);
+        this(text, false);
     }
-    
-    int i = 0;
+
+    public VerticalLabel(String text, boolean clockwise) {
+        super(text);
+        this.clockwise = clockwise;
+//        Graphics g = getGraphics();
+//        FontMetrics metrics = getFontMetrics(g.getFont());
+//        Rectangle2D bounds = metrics.getStringBounds(getText(), g);
+//        w = bounds.getWidth();
+//        h = bounds.getHeight();
+    }
+
+//    @Override
+//    public int getWidth() {
+//        return super.getHeight();
+//    }
+//
+//    // ^ v returning oppisite things cause its rotated.
+//    @Override
+//    public int getHeight() {
+//        return super.getWidth();
+//    }
 
     @Override
     public void paintComponent(Graphics g) {
-        FontMetrics metrics = getFontMetrics(g.getFont());
-        Rectangle2D bounds = metrics.getStringBounds(getText(), getGraphics());
-        Graphics2D gx = (Graphics2D) g;
-        int halfX = (int) ((bounds.getWidth() / 2f));
-        int halfY = (int) ((bounds.getHeight() / 2f));
-        Rectangle rotatedRec = new Rectangle(halfX-halfY, halfY-halfX, (int) bounds.getHeight(), (int) bounds.getWidth());
-        gx.setColor(Color.BLUE);
-        gx.setClip(rotatedRec);
+        Graphics2D gx = (Graphics2D) g.create();
+
+        gx.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         
-        gx.rotate(Math.toRadians(-90), halfX, halfY);
-       // gx.drawLine(0, 0, halfX, halfY);
-        super.paintComponent(gx);
+        FontMetrics metrics = getFontMetrics(gx.getFont());
+        int textWid = metrics.stringWidth(getText());
+        int textHei = metrics.getAscent();
+        float halfX = getWidth() / 2f;
+        float halfY = getHeight() / 2f;
+        gx.setClip(null);
+        
+        gx.translate(halfX,0);
+        gx.rotate(Math.toRadians(clockwise ? 90 : -90));
+        float x = -textHei / 2f;
+        float y = textWid / 2f;
+
+        if (clockwise) {
+            y = -y;
+        }
+
+        gx.drawString(getText(), 
+                x, 
+                y
+        );
+        //gx.drawString(getText(), 0,0);
+        // gx.drawLine(0, 0, halfX, halfY);
+       // super.paintComponent(gx);
+        gx.dispose();
     }
 }
